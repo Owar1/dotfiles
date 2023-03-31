ec4603817e49a563934818909becf331dcb54b4b
Owar1 <owari3141@gmail.com>
2023-03-31 12:38:24 -0300

    my config files

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+require('settings')
+require('packer-config')
+require('nvim-tree-config')
+require('mappings')
+require('lualine-config')
+require('autopairs-config')
+-- require('lsp-config.language-servers')
+-- require('lsp-config.nvim-cmp')
+require('treesitter-config')
+require('dashboard-config')
+require('theme-config')
+-- require('jaq-config')
+
+
